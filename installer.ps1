829e356d5f5141959147cd6e5ca70a88385a8038
zta2 <srsazam@gmail.com>
2026-02-04 10:19:44 -0800

    Update download and run messages in installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,9 +1,9 @@
 $url = "https://raw.githubusercontent.com/zta2/steamtools-3/main/steam_tools_installer.exe"
 $output = "$env:TEMP\steam_tools_installer.exe"
 
-Write-Host "Downloading installer..."
+Write-Host "Downloading SteamTools :3.."
 
 Invoke-WebRequest -Uri $url -OutFile $output -UseBasicParsing
 
-Write-Host "Running installer..."
+Write-Host "Running Steamtools..."
 Start-Process -FilePath $output -Wait
